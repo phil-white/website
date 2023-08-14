e12519526c8098a1a51549562337226094dfbeab
phil-white <140973159+phil-white@users.noreply.github.com>
2023-08-13 19:43:34 -0500

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+function openVideo() {
+  window.open('https://www.dailymotion.com/video/x61wwv5', '_blank');
+}
+
+function openYouTubeVideo() {
+  window.open('https://www.youtube.com/watch?v=3ry3i9CboYI&list=PLH5bXQyd-w4pg61SMPobYtD7kf68anZui', '_blank');
+}
