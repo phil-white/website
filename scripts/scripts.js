7964e0c004c63fdb6270faa2b428f49014efce33
phil-white <140973159+phil-white@users.noreply.github.com>
2023-08-19 17:03:19 -0500

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,9 @@
 function openVideo() {
-  window.open('https://www.dailymotion.com/video/x61wwv5', '_blank');
+  window.open('https://www.dailymotion.com/video/x61wwv5');
 }
 
 function openYouTubeVideo() {
-  window.open('https://www.youtube.com/watch?v=3ry3i9CboYI&list=PLH5bXQyd-w4pg61SMPobYtD7kf68anZui', '_blank');
+  window.open('https://www.youtube.com/watch?v=3ry3i9CboYI&list=PLH5bXQyd-w4pg61SMPobYtD7kf68anZui');
 }
 
 function playAudio() {
