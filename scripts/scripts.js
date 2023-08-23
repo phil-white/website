9cbbdb827cf62da3d38b89f4fa0c8c6f8443ca74
phil-white <140973159+phil-white@users.noreply.github.com>
2023-08-22 21:23:12 -0500

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+function openVideo() {
+  window.open('https://www.dailymotion.com/video/x61wwv5');
+}
+
+function openYouTubeVideo() {
+  window.open('https://www.youtube.com/watch?v=3ry3i9CboYI&list=PLH5bXQyd-w4pg61SMPobYtD7kf68anZui');
+}
+
+function playAudio() {
+  var audio = document.getElementById("audio");
+  audio.play();
+}
